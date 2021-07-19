9c8849f847e33f74798607c6323010b7b630043e
Stan Wong <swong394@gmail.com>
2021-07-19 16:24:25 -0400

    Add basic webhook main routine (with flag handling) and audit event http handler
    Signed-off-by: Stan Wong <swong394@gmail.com>
    Co-authored-by: Dhruv Jain <2dhruv@gmail.com>

---
+++ pkg/webhook/webhook.go
@@ -0,0 +1,54 @@
+// Copyright 2021 Antrea Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package webhook
+
+import (
+	"io/ioutil"
+	"net/http"
+
+	"antrea.io/resource-auditing/pkg/webhook/gitmanager"
+	"k8s.io/klog/v2"
+)
+
+func events(w http.ResponseWriter, r *http.Request, cr *gitmanager.CustomRepo) {
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		klog.ErrorS(err, "unable to read audit body")
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	klog.V(3).Infof("Audit received: %s", string(body))
+	if err := cr.HandleEventList(body); err != nil {
+		if err.Error() == "rollback-in-progress" {
+			w.WriteHeader(http.StatusServiceUnavailable)
+		} else {
+			klog.ErrorS(err, "unable to process audit event list")
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	}
+}
+
+func ReceiveEvents(dir string, port string, cr *gitmanager.CustomRepo) error {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		events(w, r, cr)
+	})
+	klog.Infof("Audit webhook server started, listening on port %s", port)
+	if err := http.ListenAndServe(":"+string(port), nil); err != nil {
+		klog.ErrorS(err, "Audit webhook service died")
+		return err
+	}
+	return nil
+}
+
